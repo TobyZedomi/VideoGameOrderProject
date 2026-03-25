a95e00b6e7d0ff1fb65f0d3be4d4fe22e791ad98
TobyZedomi <tobyzedo4@gmail.com>
2026-03-25 05:10:39 +0000

    AuthUtils

---
+++ src/main/java/client/AuthUtils.java
@@ -0,0 +1,63 @@
+package client;
+
+public class AuthUtils {
+
+    public static final String SERVER_HOST = "localhost";
+    public static final int SERVER_PORT = 11000;
+
+
+    // REQUEST
+    public static final String LOGIN = "LOGIN";
+
+    public static final String REGISTER = "REGISTER";
+
+    public static final String ORDER = "ORDER";
+
+    public static final String CANCEL = "CANCEL";
+
+    public static final String ORDERS_RETRIEVED_SUCCESSFULLY = "ORDERS_RETRIEVED_SUCCESSFULLY";
+
+    public static final String VIEW = "VIEW";
+
+    public static final String CANCELLED = "CANCELLED";
+
+    public static final String LOGOUT = "LOGOUT";
+
+    public static final String END = "END";
+
+    public static final String EXIT = "EXIT";
+
+
+    // RESPONSES
+
+    public static final String LOGIN_SUCCESSFUL = "LOGIN_SUCCESSFUL";
+    public static final String REGISTER_SUCCESSFUL =   "REGISTER_SUCCESSFUL";
+    public static final String NON_NUMERIC_ID = "NON_NUMERIC_ID";
+
+    public static final String NOT_LOGGED_IN =   "NOT_LOGGED_IN";
+
+    public static final String PRICE_NOT_VALID = "PRICE_NOT_VALID";
+
+
+    public static final String INVALID_DATE_TIME = "INVALID_DATE_TIME";
+
+    public static final String TITLE_EMPTY = "TITLE_EMPTY";
+
+    public static final String INVALID =   "INVALID";
+
+    public static final String BUYER_SELLER_NOT_VALID =   "BUYER_SELLER_NOT_VALID";
+
+    public static final String BUYER_SELLER_EMPTY =   "BUYER_SELLER_EMPTY";
+
+    public static final String NOT_FOUND =   "NOT_FOUND";
+
+    public static final String YOU_HAVE_NO_ORDERS =   "YOU_HAVE_NO_ORDERS";
+
+
+    public static final String MATCH = "MATCH";
+
+
+
+    /// Delimiter
+    public static final String DELIMITER = "%%";
+}
